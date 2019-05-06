27289acb52b242fdbf02c3abf8db27bc1ed4a638
Olga <olga.beletskaya@gmail.com>
2019-05-06 12:33:08 +1000

    Fixture added to groupCreationTest

--- addressbook-tests/src/test/java/addressbook/GroupCreationTest.java
+++ addressbook-tests/src/test/java/addressbook/GroupCreationTest.java
@@ -2,6 +2,7 @@ package addressbook;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,19 +15,30 @@ public class GroupCreationTest {
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
-  public void GroupCreationTest() {
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  public void testGroupCreation() {
+
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
     driver.findElement(By.name("group_name")).sendKeys("test1");
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
     driver.findElement(By.name("group_header")).sendKeys("test22");
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
     driver.findElement(By.name("group_footer")).sendKeys("test33");
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("groups")).click();
@@ -36,6 +48,15 @@ public class GroupCreationTest {
     public void tearDown () {
       driver.quit();
     }
+
+  public static boolean isAlertPresent(FirefoxDriver driver) {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
   }
 
 
