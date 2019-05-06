00a5073aa0fafd14f63dec3ceab3f39d1819a381
Olga <olga.beletskaya@gmail.com>
2019-05-06 16:06:13 +1000

    Refactoring for GroupCreationTest
    Extracted methods, parameters, object parameters

--- addressbook-tests/src/test/java/addressbook/GroupCreationTest.java
+++ addressbook-tests/src/test/java/addressbook/GroupCreationTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GroupCreationTest {
-  FirefoxDriver driver;
+  FirefoxDriver driver; //это attribute объекта типа GroupCreationTest
 
   @BeforeMethod
   public void setUp() throws Exception {
@@ -17,34 +17,54 @@ public class GroupCreationTest {
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
     driver.get("http://localhost/addressbook/");
+    login("admin", "secret"); //тут вызывается метод login, а не ф-я, тк используется тут переменная driver которяая является обхектом )
+  }
+
+  private void login(String user, String password) {
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("user")).sendKeys(user);
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.name("pass")).sendKeys(password);
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testGroupCreation() {
 
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test1", "test22", "test33"));
+    submitGroupCreation();
+    gotoGroupPage();
+  }
+
+  private void submitGroupCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test1");
+    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
     driver.findElement(By.name("group_header")).click();
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test22");
+    driver.findElement(By.name("group_header")).sendKeys(groupData.getHead());
     driver.findElement(By.name("group_footer")).click();
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test33");
-    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
     driver.findElement(By.linkText("groups")).click();
   }
 
-    @AfterMethod
+  @AfterMethod
     public void tearDown () {
       driver.quit();
     }
---
+++ addressbook-tests/src/test/java/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package addressbook;
+
+public class GroupData {
+    private final String name;
+    private final String head;
+    private final String footer;
+
+    public GroupData(String name, String head, String footer) { //это конструктор кот инициализирует объект значениями)
+        this.name = name;
+        this.head = head;
+        this.footer = footer;
+    }
+
+    public String getName() {
+        return name;
+    } //методы кот возвращают артибуты
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+}
