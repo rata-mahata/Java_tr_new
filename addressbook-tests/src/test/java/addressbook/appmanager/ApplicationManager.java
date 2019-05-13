d52943454ff4861acd9f066efd5f488156c03796
Olga <olga.beletskaya@gmail.com>
2019-05-13 10:08:39 +1000

    Browser as variable added
    Lecture 3.3

--- addressbook-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
@@ -2,19 +2,41 @@ package addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.openqa.selenium.remote.BrowserType.*;
+
 public class ApplicationManager {
 
-    FirefoxDriver driver; //это attribute объекта типа GroupCreationTest
+    WebDriver driver; //это attribute объекта типа GroupCreationTest
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser){
+
+        this.browser = browser;
+    }
 
 
     public void init() {
+
+        if (browser == BrowserType.FIREFOX) {
+            driver = new FirefoxDriver();
+        }
+        else if (browser == BrowserType.CHROME){
+            driver = new ChromeDriver();
+        }
+        else if (browser == BrowserType.IE){
+            driver = new InternetExplorerDriver();
+        }
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
--- addressbook-tests/src/test/java/addressbook/appmanager/SessionHelper.java
+++ addressbook-tests/src/test/java/addressbook/appmanager/SessionHelper.java
@@ -1,11 +1,12 @@
 package addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SessionHelper extends HelperBase {
 
-    public SessionHelper(FirefoxDriver driver) {
+    public SessionHelper(WebDriver driver) {
         super(driver);
 
     }
